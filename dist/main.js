b82e6c1f5c32c2c6da2ba3484452789adc4285b2
Lev <lyoha.bel2020@gmail.com>
2022-01-09 11:28:36 +0300

    feature/WP-n1 webpack configured

---
+++ dist/main.js
@@ -0,0 +1,86 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/style.scss":
+/*!************************!*\
+  !*** ./src/style.scss ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ "./src/style.scss");
+
+
+var sayHello = function sayHello() {
+  return console.log("hello!!!");
+};
+
+sayHello();
+var text1 = "Hello, friends!"; // создаем параграф
+
+var p = document.createElement("p");
+p.textContent = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam, nobis repellendus? Cupiditate iure voluptates voluptate voluptatum eligendi ut praesentium sapiente eum aliquam et rerum omnis, veritatis sunt beatae magnam minima!"; // создаем элемент заголовка
+
+var heading = document.createElement("h1");
+heading.textContent = "Wow! ".concat(text1, ".");
+heading.classList.add("title"); // добавляем параграф и заголовок в DOM
+
+var root = document.querySelector("#root");
+root.append(heading, p);
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
